d89e2704f697511312bb1832a32e30d36f5bc531
KennyLightfoot <kennylightfoot03@gmail.com>
2025-08-23 17:27:33 -0500

    fix(gtm): correct GTM script template literal formatting
    - Fix broken JavaScript template literal in GTM script that was preventing container from loading\n- Properly format GTM initialization code for reliable execution

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,11 +85,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       {process.env.NEXT_PUBLIC_GTM_ID && (
-        <Script id="gtm-base" strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`}</Script>
+        <Script id="gtm-base" strategy="afterInteractive">{`
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+        `}</Script>
       )}
       <body className={inter.className}>
         {process.env.NEXT_PUBLIC_GTM_ID && (
